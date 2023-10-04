9a7202efc73a905c556a8bf147bcfc9d5c06a3de
Alec <asant930@gmail.com>
2023-10-04 18:24:31 -0400

    finished repeat string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, numTimes) {
+  if (numTimes < 0) {
+    return "ERROR";
+  }
+  let string = "";
+  for (i = 0; i < numTimes; i++) {
+    string += word;
+  }
+  return string;
 };
 
 // Do not edit below this line
